acdb387fc0636d84d6f2c78c919fb67064679e31
DasGestirn <schwarze@tutanota.com>
2026-06-23 15:16:38 -0300

    Refatoração geral do código e mecanismo para impedir que processos inválidos sejam adicionados na tabela de agendamento

--- scripts/escalonadores/sjf.js
+++ scripts/escalonadores/sjf.js
@@ -1,25 +1,4 @@
-function lerParametros(){
-    /*
-    Entradas do simulador
-
-    Conjunto de processos P[i] com atributos:
-    Parâmetros globais:
-    Atributo Descrição
-    id Identificador único
-    chegada instante de chegada
-    execução tempo total de CPU
-    prioridade inteiro (1 = maior prioridade)
-    deadline instante máximo desejado (absoluto)
-    quantum (absoluto) - para os algoritmos preemptivos
-    sobrecarga_contexto (absoluto) - tempo gasto em cada troca de contexto
-    
-    arrayProcessos = [[nome do processo, duração do processo, chegada do processo = 0]]
-     
-    return arrayProcessos
-    */
-}
-
-function sjf(arrayProcessos, sobrecargaContexto = 0) { // <-- Adicionado o parâmetro de sobrecarga
+export function sjf(arrayProcessos, sobrecargaContexto = 0) {
     
     // Formato esperado para cada um dos processos: [PID, Chegada, Execução, Deadline, Prioridade]
     let processosRestantes = arrayProcessos.map(p => ({
@@ -95,27 +74,4 @@ function sjf(arrayProcessos, sobrecargaContexto = 0) { // <-- Adicionado o parâ
     }
     
     return { tabelaFinal, ganttCoordenadas };
-}
-
-//[[id do processo, duração do processo, tempo de chegada]]
-let processosTeste = [["a", 14, 8], ["b", 21], ["c", 6, 1], ["d", 32, 90]];
-
-
-function diagramaGantt(){
-    /*
-    Saídas Obrigatórias
-    1.​ Gráfico de Gantt:
-    a.​ verde → execução
-    b.​ vermelho → sobrecarga de contexto
-    c.​ cinza → estouro de deadline
-    d.​ linha vertical → deadline absoluto
-
-    2.​ Tabela final:
-    a.​ chegada, execucao, deadline, prioridade, inicio(s), termino, espera, turnaround,
-    deadline_ok?.
-
-    3.​ Resumo quantitativo:
-    a.​ médias, throughput, % ociosidade, total de trocas de contexto.
-    b.​ Modo passo-a-passo (opcional, p/ demonstração): atraso visual entre eventos.
-    */
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,18 @@
 'use strict';
 
+// Importando escalonadores
+import { sjf } from './escalonadores/sjf.js';
+
 // Elementos da configuração do escalonador | Entradas do usuário
 
-let tempoChegada;
-let tempoExecucao;
-let quantum;
-let prioridade;          
-let deadline;            
-let sobrecargaContexto;
-
-/* const configuracaoEscalonador = {
-    tempochegada: null,
-    tempoexecucao: null,
+const configuracaoEscalonador = {
+    tempoChegada: null,
+    tempoExecucao: null,
     quantum: null,
     prioridade: null,
     deadline: null,
-    sobrecargacontexto: null
-}; */
+    sobrecargaContexto: null
+}; 
 
 const entradasInput = document.querySelectorAll('.entradas')
 
@@ -24,41 +20,14 @@ const entradasInput = document.querySelectorAll('.entradas')
 
 entradasInput.forEach(entrada => {
     entrada.addEventListener('change', function(e) {
-/* 
+
         const id = e.target.id;
 
         if (id in configuracaoEscalonador) {
-            configuracaoEscalonador
-        } */
+            configuracaoEscalonador[id] = Number(e.target.value)
+        } 
 
-    // TODO: Remover esse monte de if | Pretendo fazer isso com o objeto configuracaoEscalonador
-
-
-    if (e.target.id === 'tempoChegada') {
-        tempoChegada = Number(document.getElementById('tempoChegada').value)
-    }
-
-    if (e.target.id === 'tempoExecucao') {
-        tempoExecucao = Number(document.getElementById('tempoExecucao').value)
-    }
-
-    if (e.target.id === 'quantum') {
-        quantum = Number(document.getElementById('quantum').value)
-    }
-
-    if (e.target.id === 'prioridade') {
-            prioridade = Number(document.getElementById('prioridade').value);
-        }
-
-        if (e.target.id === 'deadline') { 
-            deadline = Number(document.getElementById('deadline').value);
-        }
-
-        if (e.target.id === 'sobrecargacontexto') {
-            sobrecargaContexto = Number(document.getElementById('sobrecargacontexto').value);
-        }
-
-        console.log(`Chegada: ${tempoChegada} | Execução: ${tempoExecucao} | Quantum: ${quantum} | Prioridade: ${prioridade} | Deadline: ${deadline} | Sobrecarga: ${sobrecargaContexto}`);
+        console.log(configuracaoEscalonador);
 })
 })
 
@@ -68,7 +37,7 @@ entradasInput.forEach(entrada => {
 
 // Array dos processos atuais para posterior processamento
 
-const arrayProcessosTabela = [];
+let arrayProcessosTabela = [];
 
 ///// Elementos
 
@@ -99,29 +68,111 @@ function htmlProcesso(id,chegada,execucao,deadline,prioridade,termino,espera,tur
 
 // Remove o último processo presente na tabela de agendamento
 removerProcesso.addEventListener('click', function() {
+
+    if (corpoTabela.childElementCount === 0) {
+        window.alert("Não há processos para remover!")
+        return
+    }
     corpoTabela.lastElementChild?.remove();
     arrayProcessosTabela.pop();
     console.log(arrayProcessosTabela);
 })
 
 // Adiciona os processos somente com o tempo de chegada e execução na tabela de agendamento
-adicionarProcesso.addEventListener('click', function () {
+
+adicionarProcesso.addEventListener('click', function (e) {
+
+    // Impedir o processo de ser adicionado se possuir input vazio para cada tipo de escalonador
+
+    if (configuracaoEscalonador.tempoChegada === null || configuracaoEscalonador.tempoExecucao === null) {
+        window.alert("Erro: O Tempo de Chegada e o Tempo de Execução são obrigatórios!")
+        return
+    }
+
+    const algoritmoSelecionado = e.target.dataset.algoritmo
+
+    switch(algoritmoSelecionado){
+
+        case "rr":
+            if (configuracaoEscalonador.quantum === null || configuracaoEscalonador.quantum <= 0) {
+                window.alert("Erro: O algoritmo Round Robin exige um valor válido para o Quantum!");
+                return;
+            }
+            break;
+
+        case "prioridade":
+            if (configuracaoEscalonador.prioridade === null) {
+                alert("Erro: Informe a Prioridade para adicionar o processo.");
+                return;
+            }
+            break;
+
+        case "edf":
+            if (configuracaoEscalonador.deadline === null) {
+                alert("Erro: O algoritmo EDF exige que o Deadline seja informado!");
+                return;
+            }
+            break;
+
+        case "sjf":
+        case "fcfs":
+            break;
+            
+        default:
+            console.error("Algoritmo desconhecido selecionado.");
+            return;
+    }
+
     // Se o processo a ser adicionado for o primeiro, o ID será 1.
     // Se não for o primeiro, o id do processo a ser adicionado será o anterior incrementado por 1.
     const processoID = corpoTabela.childElementCount === 0 ? 1 : Number(Array.from(corpoTabela.children)[Array.from(corpoTabela.children).length - 1].children[0].textContent.slice(1)) + 1;
 
     arrayProcessosTabela.push({
         id: processoID,
-        chegada: tempoChegada,
-        execucao: tempoExecucao,
+        chegada: configuracaoEscalonador.tempoChegada,
+        execucao: configuracaoEscalonador.tempoExecucao,
+        prioridade: configuracaoEscalonador.prioridade,
+        deadline: configuracaoEscalonador.deadline
     })
 
-    const processoStart = htmlProcesso(processoID,tempoChegada,tempoExecucao,'','','','','','');
+    const processoStart = htmlProcesso(processoID,
+        configuracaoEscalonador.tempoChegada,
+        configuracaoEscalonador.tempoExecucao,
+        configuracaoEscalonador.deadline || '-', 
+        configuracaoEscalonador.prioridade || '-', 
+        '-', '-', '-', '-');
 
     corpoTabela.insertAdjacentHTML('beforeend', processoStart);
 
     console.log(arrayProcessosTabela);
 
 })
+
+// Acionar simulação do escalonamento 
+
+simularEscalonador.addEventListener('click', function() {
+
+    if (arrayProcessosTabela.length === 0) {
+        alert("Adicione pelo menos um processo para simular!");
+        return;
+    }
+
+    const processosParaAlgoritmo = arrayProcessosTabela.map(proc => {
+        return [
+            proc.id, 
+            proc.chegada, 
+            proc.execucao, 
+            configuracaoEscalonador.deadline || Infinity, 
+            configuracaoEscalonador.prioridade || 0
+        ];
+    });
+
+    const sobrecarga = configuracaoEscalonador.sobrecargaContexto || 0;
+
+    const resultadoSimulacao = sjf(processosParaAlgoritmo, sobrecarga);
+
+    console.log("Resultado Matemático:", resultadoSimulacao);
+
+});
 
 ///// ==========================================
