b68e88cde140f9cda8da53060c5cc21ccfecfad7
Lincoln <lincoln007silva@gmail.com>
2026-06-26 02:57:28 -0300

    feat(lincoln-pereira): inicia a implementação do diagrama de gantt e algumas correções

--- scripts/escalonadores/fcfs.js
+++ scripts/escalonadores/fcfs.js
@@ -1,35 +1,14 @@
-function lerParametros() {
-  /*
-  Entradas do simulador
-
-  Conjunto de processos P[i] com atributos:
-  Parâmetros globais:
-  Atributo Descrição
-  id Identificador único
-  chegada instante de chegada
-  execução tempo total de CPU
-  prioridade inteiro (1 = maior prioridade)
-  deadline instante máximo desejado (absoluto)
-  quantum (absoluto) - para os algoritmos preemptivos
-  sobrecarga_contexto (absoluto) - tempo gasto em cada troca de contexto
-  
-  arrayProcessos = [[nome do processo, duração do processo, chegada do processo = 0]]
-   
-  return arrayProcessos
-  */
-}
-
 export function fcfs(arrayProcessos) {
   arrayProcessos.sort((a, b) => (a[2] ?? 0) - (b[2] ?? 0))
 
   let ganttCoordenadas = []; //[nome, inicio, fim]
   for (let i = 0; i < arrayProcessos.length; i++) {
-    idProcesso = arrayProcessos[i][0];
+    let idProcesso = arrayProcessos[i][0];
 
     let inicioProcesso = (i == 0) ? 0 : ganttCoordenadas[i - 1][2];
     inicioProcesso = (arrayProcessos[i][2] > inicioProcesso) ? arrayProcessos[i][2] : inicioProcesso;
 
-    fimProcesso = inicioProcesso + arrayProcessos[i][1];
+    let fimProcesso = inicioProcesso + arrayProcessos[i][1];
 
     ganttCoordenadas.push([idProcesso, inicioProcesso, fimProcesso])
   }
@@ -37,26 +16,84 @@ export function fcfs(arrayProcessos) {
 }
 
 //[[id do processo, duração do processo, tempo de chegada]]
-let processosTeste = [["a", 14, 8], ["b", 21], ["c", 6, 1], ["d", 32, 90]];
-
-// console.log(fcfs(processosTeste));
+let processosTeste = [[1, 14, 8], [2, 21], [3, 6, 1], [4, 32, 90]];
 
 
-function diagramaGantt() {
-  /*
-  Saídas Obrigatórias
-  1.​ Gráfico de Gantt:
-  a.​ verde → execução
-  b.​ vermelho → sobrecarga de contexto
-  c.​ cinza → estouro de deadline
-  d.​ linha vertical → deadline absoluto
+let processes = fcfs(processosTeste)
 
-  2.​ Tabela final:
-  a.​ chegada, execucao, deadline, prioridade, inicio(s), termino, espera, turnaround,
-  deadline_ok?.
+console.log(processes)
 
-  3.​ Resumo quantitativo:
-  a.​ médias, throughput, % ociosidade, total de trocas de contexto.
-  b.​ Modo passo-a-passo (opcional, p/ demonstração): atraso visual entre eventos.
-  */
-}
+// function ganttChart() {
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+  Highcharts.chart('gantt-chart', {
+    chart: {
+      type: 'gantt',
+      backgroundColor: 'hsl(0, 0%, 100%)',
+      height: 400,
+      width: null
+    },
+    title: {
+      text: ''
+    },
+    xAxis: {
+      min: 0,
+      max: processes.at(-1)[3],
+      tickInterval: 5,
+      gridLineWidth: 1,
+      // plotLines: [                // Linhas verticais -- DEADLINE
+      //   {
+      //     value: 3,           // Onde a linha vai passar no eixo X
+      //     color: 'red',
+      //     width: 2,
+      //     zIndex: 5           // Garante que fica por cima dos blocos
+      //   }
+      // ]
+    },
+    yAxis: {
+      title: '',
+      categories: processes.map(proc => `ID: ${proc[0]}`),
+      gridLineWidth: 1
+    },
+    legend: {
+      enabled: false,
+    },
+    plotOptions: {
+      series: {
+        pointPadding: 0.1,
+        groupPadding: 0.1
+      }
+    },
+    series: [{
+      name: 'FCFS', // Nome da série para a legenda
+      data: processes.flatMap(proc => [
+        {
+          name: `ID: ${proc[0]}`,
+          start: proc[1],
+          end: proc[2],
+          y: proc[0] - 1,
+          color: 'green',
+        }
+        // TODO: TERMINAR TROCA DE CONTEXTO
+        // {
+        //   name: 'Troca de Contexto',
+        //   start: proc[2],
+        //   end: proc[2] + 1,
+        //   y: proc[0] - 1,
+        //   color: 'red',
+        // }
+      ])
+    }],
+    tooltip: {
+      formatter: function () {
+        return '<b>' + this.point.name + '</b><br/>' +
+          'Início: ' + this.point.start + '<br/>' +
+          'Fim: ' + this.point.end;
+      }
+    },
+    credits: {
+      enabled: false
+    }
+  });
+});
